0153b4adb9f86b2d0487e835c3dabdd08b80d5b4
Elder Viana <hedervianadasilva@gmail.com>
2024-03-23 13:49:56 -0300

    Atualizei

--- diet2.js
+++ diet2.js
@@ -1,16 +1,22 @@
-const input = document.querySelectorAll(["input"]);
-let loseWeight = input.loseWeight
-if(loseWeight === true){
-	console.log(2 + 2);
-}
+const input = document.querySelectorAll("[input]");
+const loseWeight = document.getElementById("loseWeight")
+const keepMuscleMass = document.getElementById("keepMuscleMass")
+const gainMuscleMass = document.getElementById("gainMuscleMass")
 
 
 
 
 
-
-
-
+const chooseMenu = () => {
+	for(let i = 0; i < input.length; i++){															//console.log(input[0].checked)
+		if(loseWeight.checked === true){
+			if(input[0].checked || input[1].checked === true){
+				console.log("hello");
+			}
+		}
+console.log("oi");
+	}
+};
 
 
 
@@ -130,3 +136,55 @@ for( i = 0; i < foods.length; i++){
 
 }
 
+
+/*
+
+let loseWeight = input.id === "input-loseWeight" && input.checked === true;
+let keepWeight = input.id === "input-keepWeight" && input.checked === true;
+let gainWeight = input.id === "input-gainWeight" && input.checked === true;
+
+let wholemealBread = input.id === "input-wholemealBread"&& input.cheched === true;
+let tapioca = input.id === "input-tapioca"&& input.cheched === true;
+let sugar = input.id === "input-sugar"&& input.cheched === true;
+let egg = input.id === "input-egg"&& input.cheched === true;
+let ricotaCheese = input.id === "input-ricotacheese"&& input.cheched === true;
+let skimedMilk = input.id === "input-skimedMilk"&& input.cheched === true;
+
+let banana = input.id === "input-banana"&& input.cheched === true;
+let oat = input.id === "input-oat"&& input.cheched === true;
+let peanutButter = input.id === "input-peanutButter"&& input.cheched === true;
+let yakult = input.id === "input-yakult"&& input.cheched === true;
+let naturalYogurt = input.id === "input-naturalYogurt"&& input.cheched === true;
+
+let sweetPotato = input.id === "input-sweetPotato"&& input.cheched === true;
+
+let whey = input.id === "input-whey"&& input.cheched === true;
+
+let wholegrainRice = input.id === "input-wholegrainRice"&& input.cheched === true;
+let cassava = input.id === "input-cassava"&& input.cheched === true;
+let chickenFillet = input.id === "input-chickenFillet"&& input.cheched === true;
+let tilapiaFillet = input.id === "input-tilapiaFillet"&& input.cheched === true;
+let porkChop = input.id === "input-porkChop"&& input.cheched === true;
+let bean = input.id === "input-bean"&& input.cheched === true;
+let tomato = input.id === "input-tomato"&& input.cheched === true;
+let cabbage = input.id === "input-cabbage"&& input.cheched === true;
+let carrot = input.id === "input-carrot"&& input.cheched === true;
+
+let apple = input.id === "input-apple"&& input.cheched === true;
+let sleeve = input.id === "input-sleeve"&& input.cheched === true;
+let persimmon = input.id === "input-persimmon"&& input.cheched === true;
+let starFruit = input.id === "input-starFruit"&& input.cheched === true;
+let orange = input.id === "input-orange"&& input.cheched === true;
+
+let beefSteak = input.id === "input-beefSteak"&& input.cheched === true;
+let chckenLiver = input.id === "input-chckenLiver"&& input.cheched === true;
+let cannedSardine = input.id === "input-cannedSardine"&& input.cheched === true;
+let couscous = input.id === "input-couscous"&& input.cheched === true;
+let lettuce = input.id === "input-lettuce"&& input.cheched === true;
+let cucumber = input.id === "input-cucumber"&& input.cheched === true;
+let beet = input.id === "input-beet"&& input.cheched === true;
+let oliveOil = input.id === "input-oliveOil"&& input.cheched === true;
+
+let avocado = input.id === "input-avocado"&& input.cheched === true;
+
+*/
